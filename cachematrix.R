d616d4dc670ff6ecce5193cf26278100420a055e
SOH TSAI TZE JEREMY <jemaxisg@yahoo.com.sg>
2015-02-22 19:45:15 +0800

    final submission for assessment

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,15 @@
-## There are two functions in this script - makeCacheMatrix and cacheSolve.
-## The main purpose is to efficiently calculate the inverse of a square matrix.
-## The assumption made is that the matrix provided is always invertible.
+# There are two functions in this script - makeCacheMatrix and cacheSolve.
+# The main purpose is to efficiently calculate the inverse of a square matrix 
+# by caching its inverse.
 
-## The makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
+# The makeCacheMatrix function creates a special "matrix" object that can cache 
+# its inverse. It returns a list containing a function to:
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the inverse of the matrix
+# 4. get the inverse of the matrix
+# The assumption made is that the matrix provided is always invertible.
+
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
   set <- function(y){
@@ -18,7 +25,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 # The cacheSolve function  computes the inverse of the special "matrix" 
 # returned by makeCacheMatrix. If the inverse has already been calculated 
-# and the matrix has not changed), then the cachesolve should retrieve the 
+# and the matrix has not changed, then the cacheSolve function will retrieve the 
 # inverse from the cache.
 
 cacheSolve <- function(x, ...) {
